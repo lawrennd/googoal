8d0d6751dd436c75f72fd3d1df2ef6ed0f27c839
Neil Lawrence <lawrennd@gmail.com>
2022-01-05 15:23:31 +0000

    Start to take out the credentials

--- googoal/googoal.py
+++ googoal/googoal.py
@@ -4,27 +4,22 @@ import json
 
 from .config import *
 
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
 if "google" in config:
     if "oauth2_keyfile" in config["google"]:  # Check if config file is set up
-        keyfile = os.path.expanduser(
+        KEYFILE = os.path.expanduser(
             os.path.expandvars(config["google"]["oauth2_keyfile"])
         )
     else:
-        keyfile = None
+        KEYFILE = None
 else:
-    keyfile = None
-
-NEW_OAUTH2CLIENT = False
-try:
-    # See this change: https://github.com/google/oauth2client/issues/401
-    from oauth2client.service_account import ServiceAccountCredentials
-
-    NEW_OAUTH2CLIENT = True
-except ImportError:
-    try:
-        from oauth2client.client import SignedJwtAssertionCredentials
-    except ImportError:
-        api_available = False
+    KEYFILE = None
 
 
 class Google_service:
@@ -32,39 +27,46 @@ class Google_service:
 
     # Get a google API connection.
     def __init__(self, scope=None, credentials=None, http=None, service=None):
-        if service is None:
-            if http is None:
-                if credentials is None:
-                    if keyfile is not None:
-                        if os.path.exists(keyfile):
-                            with open(keyfile) as file:
-                                self._oauthkey = json.load(file)
-                            self.email = self._oauthkey["client_email"]
-                            self.key = bytes(self._oauthkey["private_key"], "UTF-8")
-                            self.scope = scope
-
-                            if NEW_OAUTH2CLIENT:
-                                self.credentials = ServiceAccountCredentials.from_json_keyfile_name(
-                                    os.path.join(keyfile), self.scope
-                                )
-                                # self.credentials = ServiceAccountCredentials.from_p12_keyfile(self.email, self.keyos.path.join(keyfile), self.scope)
-                            else:
-                                self.credentials = SignedJwtAssertionCredentials(
-                                    self.email, self.key, self.scope
-                                )
-                        else:
-                            raise FileNotFoundError("keyfile " + file + " not found, update its location in _googoal.yml")
-                    else:
-                        raise Exception("No keyfile entry provided in config file for OAuth 2.0 access, see https://developers.google.com/identity/protocols/oauth2 to find out how to generate the file and place entry in _googoal.yml")
-                else:
-                    self.credentials = credentials
-                    self.key = None
-                    self.email = None
-
-                http = httplib2.Http()
-                self.http = self.credentials.authorize(http)
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if credentials is None:
+            if os.path.exists("token.json"):
+                self.credentials = Credentials.from_authorized_user_file("token.json", SCOPES)
             else:
-                self.http = http
+                self.credentials = False
+        else:
+            self.credentials = credentials
+
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())
+            else:
+                if KEYFILE is not None:
+                    if os.path.exists(KEYFILE):
+                        self.flow = InstalledAppFlow.from_client_secrets_file(
+                            KEYFILE,
+                            SCOPES
+                        )
+                        self.credentials = self.flow.run_local_server(port=0)
+                        # Save credentials for next run
+                        with open("token.json", "w") as token:
+                            token.write(self.credentials.to_json())
+                    else:
+                        raise FileNotFoundError("keyfile " + KEYFILE + " not found, update its location in _googoal.yml")
+                else:
+                    raise Exception("No keyfile entry provided in config file for OAuth 2.0 access, see https://developers.google.com/identity/protocols/oauth2 to find out how to generate the file and place entry in _googoal.yml")
+        if service is None:
+            try:
+                self.service = build("drive", "v3", credentials=self.credentials)
+            except HttpError as error:
+                raise Exception(f"Cannot access service: {error}")
         else:
             self.service = service
+            
+        if http is None:
+            http = httplib2.Http()
+            self.http = self.credentials.authorize(http)
+        else:
+            self.http = http
 
